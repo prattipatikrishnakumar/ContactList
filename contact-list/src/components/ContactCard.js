647ffbe2549371d14dd99eb8fa72331885afb829
krishna <krishnakumarprathipati@gmail.com>
2022-01-24 20:02:07 +0530

    Delete Button Not Working Here

--- contact-list/src/components/ContactCard.js
+++ contact-list/src/components/ContactCard.js
@@ -12,12 +12,12 @@ export default function ContactCard(props) {
       <div>Name : {props.contact[0].Name}</div>
       <div>Email : {props.contact[0].Email}</div>
       <Stack direction="row" spacing={2}>
-        {/* <Button  variant="outlined" startIcon={<DeleteIcon />} onClick={()=>props.onDeleteClickHandler(props.contact.id)}>
+        <Button  variant="outlined" startIcon={<DeleteIcon />} onClick={()=>props.onDeleteClickHandler(props.contact.id)}>
           Delete
-        </Button> */}
-        <Button onClick={()=>props.onDeleteClickHandler(props.contact.id)} variant="outlined" >
-          Delete Contact
         </Button>
+        {/* <Button onClick={()=>props.onDeleteClickHandler(props.contact.id)} variant="outlined" >
+          Delete Contact
+        </Button> */}
 
         {/* <IconButton aria-label="delete">
         <DeleteIcon />
