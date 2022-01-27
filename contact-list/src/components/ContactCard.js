257d5550309e3a8e334dfef2b0e50241f7e5d772
krishna <krishnakumarprathipati@gmail.com>
2022-01-27 13:57:44 +0530

    Header.JS is Not Working

--- contact-list/src/components/ContactCard.js
+++ contact-list/src/components/ContactCard.js
@@ -1,35 +1,99 @@
+import * as React from 'react';
 import Button from '@mui/material/Button';
 import DeleteIcon from '@mui/icons-material/Delete';
 import Stack from '@mui/material/Stack';
 
 import IconButton from '@mui/material/IconButton';
+import {BrowserRouter as Router,Link,Routes,Route,useNavigate} from 'react-router-dom';
 
 
-export default function ContactCard(props) {
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+
+import ContactDetails from './ContactDetails';
+import { useLocation } from "react-router";
+import EditContact from './EditContact';
+
+function Item(props) {
+  const { sx, ...other } = props;
   return (
-    <>
-    <div>
-      <div>Name : {props.contact[0].Name}</div>
-      <div>Email : {props.contact[0].Email}</div>
-      <Stack direction="row" spacing={2}>
-        <Button  variant="outlined" startIcon={<DeleteIcon />} onClick={()=>props.onDeleteClickHandler(props.contact.id)}>
-          Delete
-        </Button>
-        {/* <Button onClick={()=>props.onDeleteClickHandler(props.contact.id)} variant="outlined" >
-          Delete Contact
-        </Button> */}
-
-        {/* <IconButton aria-label="delete">
-        <DeleteIcon />
-      </IconButton> */}
-      </Stack>
-      
-      
-    </div>
-    
-  </>
+    <Box
+      sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
+        border: '1px solid',
+        borderColor: (theme) =>
+          theme.palette.mode === 'dark' ? 'white' : 'white',
+        p: 1,
+        m: 1,
+        borderRadius: 2,
+        fontSize: '0.875rem',
+        fontWeight: '700',
+        ...sx,
+      }}
+      {...other}
+    />
   );
 }
 
+Item.propTypes = {
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+};
 
+
+
+// let data = useLocation();
+
+
+
+export default function ContactCard(props) {
+  const navigate = useNavigate();
+  // console.log(props.contact)
+  return (
+    <>
+ 
+
+    <div style={{ width: '100%' }}>
+      <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)' }}>
+      {/* <Link to={{pathname:`/contactDetails/${props.contact.id}`, state:{contact:props.contact}}}> */}
+
+      <Item>{props.contact.id}</Item>
+      
+      <Item>  
+        
+          <Link to={`/contactDetails/${props.contact.id}` } state={{ state: props.contact }} >{props.contact.Name}</Link>
+    
+    {/* <Link to={{
+    pathname: `/contactDetails/${props.contact.id}`,
+    state: {contact:props.contact}
+  }}>{props.contact[0].Name}</Link> */}
+    
+    </Item>
+      <Item>{props.contact.Email}</Item>
+
+      <Item><Link to={`/editContact`} state={{contact:props.contact}} ><Button  variant="outlined" color="primary"  size='small'>
+          Edit Contact
+      </Button></Link></Item>
+
+
+      <Item><Button onClick={()=>props.onDeleteClickHandler(props.contact.id)} variant="outlined" color="error"  size='small'>
+          Delete Contact
+      </Button></Item>
+
+
+
+        {/* <Item><Button  variant="outlined" startIcon={ <DeleteIcon />} onClick={()=>props.onDeleteClickHandler(props.contact.id)}>
+       Delete Contact
+        </Button></Item> */}
+      </Box>
+    </div>
+  </>
+  );
+}
 
--- contact-list/src/components/ContactList.js
+++ contact-list/src/components/ContactList.js
@@ -1,19 +1,80 @@
-import React from 'react';
+
 import ContactCard from './ContactCard'
+
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import Stack from '@mui/material/Stack';
+
+import IconButton from '@mui/material/IconButton';
+
+
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Box from '@mui/material/Box';
+import {BrowserRouter as Router,Link,Routes,Route,useNavigate} from 'react-router-dom';
+
+function Item(props) {
+  const { sx, ...other } = props;
+  return (
+    <Box
+      sx={{
+        bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#101010' : '#fff'),
+        color: (theme) => (theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800'),
+        border: '1px solid',
+        borderColor: (theme) =>
+          theme.palette.mode === 'dark' ? 'white' : 'white',
+        p: 1,
+        m: 1,
+        borderRadius: 2,
+        fontSize: '0.875rem',
+        fontWeight: '700',
+        ...sx,
+      }}
+      {...other}
+    />
+  );
+}
+
+Item.propTypes = {
+  sx: PropTypes.oneOfType([
+    PropTypes.arrayOf(
+      PropTypes.oneOfType([PropTypes.func, PropTypes.object, PropTypes.bool]),
+    ),
+    PropTypes.func,
+    PropTypes.object,
+  ]),
+};
+
+
+
+
 
 export default function ContactList(props) 
 {
 
-  console.log(props.contacts);
-  const contactsList= props.contacts.map((contact)=>{
+  console.log(props);
+
+  const navigate = useNavigate();
+  const contactsList= props.contacts.length>0?props.contacts.map((contact)=>{
    return  <ContactCard key ={contact.id} onDeleteClickHandler = {props.onDeleteClickHandler} contact={contact}/>
             
-  })
+  }):<p> No Contacts To Show!!..</p>
+
   return (
       <>
-        <div>
-         {contactsList}
-        </div>
+      <div style={{ width: '100%' }}>
+        <Box sx={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)' }}>
+          <Item><div><h2>SNO</h2></div></Item>
+          <Item><div><h2>NAME</h2></div></Item>
+          <Item><div><h2>EMAIL ADDRESS</h2></div></Item>
+          <Item></Item>
+          <Item><div><Button variant="contained" size="medium" onClick={()=>navigate("/addContact")}>Add Contact</Button></div></Item>
+        </Box>
+        <>
+        {contactsList} 
+        </>
+    </div>
+
       </>
     );
 }
